7ea6c81daf6ff1b118d78e98294fd3f4c987374d
ijd1236 <kjy960219@gmail.com>
2023-05-30 18:11:46 +0900

    as

---
+++ aaa.py
@@ -0,0 +1,2 @@
+import tensorflow as tf
+print (tf.__version__)                # 텐서플로 버전확인
---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import os 
+import pandas as pd
+from datetime import datetime
+import tensorflow as tf
+
+
+
+def main():  #화면에 나타낼것
+    st.title('내 앱 대시보드') 
+
+
+if __name__ == '__main__':
+    main()
