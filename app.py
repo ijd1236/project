513347063348b16dbe9cd72b4c3eca0dde89ba93
ijd1236 <kjy960219@gmail.com>
2023-05-31 12:48:54 +0900

    aa

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import os
 import pandas as pd
 from datetime import datetime
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL import Image #이미지 처리하는 라이브러리
 
 
+
+import streamlit as st
 
 def main():  #화면에 나타낼것
     st.title('내 앱 대시보드') 
